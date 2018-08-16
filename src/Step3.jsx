d16b8f5e8d401e6d07ced3efbbc18c85b1a46779
Tony Mamacos tony@tutuka.com <tony@tutuka.com>
2018-08-16 13:36:22 +0200

    Moved Solution to Github readme

--- src/Step3.jsx
+++ src/Step3.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Step3 extends React.Component {
     render() {
         return (
-            <a href="REAMDE.htm">REAMDE</a>
+            <a href="https://github.com/tonym128/monero-badge">Listed on GitHub README</a>
         );
     }
 }
